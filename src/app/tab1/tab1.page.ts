ea6d94f94db3ecf7418d5db24c528965188f7238
Lea <oleay001@edu.xamk.fi>
2019-04-05 14:23:55 +0300

    05042019.2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { paikkakunnat } from '../kunnat';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ModalController, MenuController } from '@ionic/angular';
+import { PikahakuPage } from '../pikahaku/pikahaku.page';
 
 @Component({
   selector: 'app-tab1',
@@ -10,22 +11,32 @@ import { AlertController } from '@ionic/angular';
 export class Tab1Page {
   paikkakunta : any;
 
-  constructor(private alertCtrl : AlertController){
+  constructor(private alertCtrl : AlertController, private modalCtrl : ModalController){
     this.paikkakunta = paikkakunnat;
   }
 
   pikahaku = async () : Promise<any> =>{
+    const modal = await this.modalCtrl.create({
+      component: PikahakuPage,
+      componentProps: {
+      }
+    });
+
+    await modal.present();
+  }
+
+  info = async () : Promise<any> =>{
     const ikkuna = await this.alertCtrl.create({
-      header: 'Alert',
-      subHeader: 'Subtitle',
-      message: 'This is an alert message.',
-      buttons: ['Cancel', 'Open Modal', 'Delete']
+      header: 'Info',
+      message: 'Valitse hakuusi sopivat määritelmä alla olevista kentistä ja paina hae. Voit myös hakea yrityksiä jotka ovat lähellä sinua painamalla lähellä sinua napilla.',
+      buttons: [
+      {
+        text:"Takaisin",
+        role:"cancel"
+      }
+    ]
     });
 
     await ikkuna.present();
-  }
-
-  info = () =>{
-
   }
 }
